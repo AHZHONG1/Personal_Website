e0d7cd694bde0730e05e3b35bbee1fb6f827cb85
AHZHONG <leolai0224@gmail.com>
2023-04-19 22:30:27 +0800

    Updating Main and Newest Update

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,7 +8,14 @@ function Home() {
                 <Header position='Home' />
             </div>
             <div className="selfDefinedBody">
-                This is home page
+                <div className="card text-center fs-2 selfDefinedSize">
+                    <img src="../../public/images/main_image.jpg" className="card-img" />
+                    <div className="card-img-overlay">
+                        <div className="card-body">
+                            <div className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur labore excepturi nemo reprehenderit dolorum. Iusto?</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Pages/NewestUpdate.tsx
+++ src/Pages/NewestUpdate.tsx
@@ -3,11 +3,33 @@ import Header from "../components/Header";
 function NewestUpdate() {
     return (
         <div>
-            <Header position="NewestUpdate"/>
+            <Header position="NewestUpdate" />
             <div>
             </div>
             <div className="selfDefinedBody">
-                This is Newest Update
+                <div className="text-center fs-3">Newest Update</div>
+                <table className="table table-info table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col" className="w-25">Date</th>
+                            <th scope="col" className="w-75">Event</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>2023-4-19</td>
+                            <td>Creating Website</td>
+                        </tr>
+                        <tr>
+                            <td>2023-4-19</td>
+                            <td>Creating Website</td>
+                        </tr>
+                        <tr>
+                            <td>2023-4-19</td>
+                            <td>Creating Website</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
