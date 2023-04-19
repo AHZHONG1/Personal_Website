d9d4d8f4c7a2d18782ddd3423d50d1732c9f1652
AHZHONG <leolai0224@gmail.com>
2023-04-19 22:50:10 +0800

    Image

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import './Home.css'
+import MainImg from "../../public/images/main_image.jpg"
 
 function Home() {
     return (
@@ -9,7 +10,7 @@ function Home() {
             </div>
             <div className="selfDefinedBody">
                 <div className="card text-center fs-2 selfDefinedSize">
-                    <img src="../../public/images/main_image.jpg" className="card-img" />
+                    <img src={MainImg} className="card-img" />
                     <div className="card-img-overlay">
                         <div className="card-body">
                             <div className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur labore excepturi nemo reprehenderit dolorum. Iusto?</div>
